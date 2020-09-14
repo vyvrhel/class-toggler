60030a5be787a5bdb76f8f0273428ea447c8bc97
Martin Bulín <bulin@itstudio.cz>
2020-09-14 16:14:20 +0200

    WIP demos

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const eslint = require('gulp-eslint');
 const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
+const twig = require('gulp-twig');
+const htmlbeautify = require('gulp-html-beautify');
 
 function js() {
   return gulp.src('src/js/**/*.js')
@@ -30,7 +32,22 @@ function lint(cb) {
   return gulp.parallel(lintJs)(cb);
 }
 
+function demos() {
+    return gulp.src([
+      'src/demos/view/*.twig',
+      '!src/demos/view/@*.twig',
+    ]).pipe(twig({
+        base: 'src/demos/view/',
+      }))
+      .pipe(htmlbeautify({
+        indent_size: 2,
+        wrap_attributes_indent_size: 2,
+      }))
+      .pipe(gulp.dest('demos'));
+}
+
 exports.js = js;
 exports.lintJs = lintJs;
 exports.lint = lint;
-exports.build = gulp.parallel(js, lint);
+exports.demos = demos;
+exports.build = gulp.parallel(js, demos, lint);
